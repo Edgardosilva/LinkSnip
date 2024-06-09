39019d99641ae1d1be614558d07823dba69e2a92
Edgardosilva <edgardosilva.es@gmail.com>
2024-06-09 15:52:08 -0400

    chore: Update MySQL connection configuration and initialization

--- database/db.js
+++ database/db.js
@@ -1,13 +1,22 @@
 import mysql from 'mysql2/promise';
 
+let db;
 
-const db = await mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',       
-  password: process.env.DB_PASSWORD || 'root',   
-  database: process.env.DB_NAME || 'urlshortener'   
-});
+async function initializeDatabase() {
+  try {
+    db = await mysql.createConnection({
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_NAME || 'urlshortener',
+      connectTimeout: 10000 // Tiempo de espera en milisegundos
+    });
+    console.log('Conectado a MySQL');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  }
+}
 
-console.log('Conectado a MySQL');
+await initializeDatabase();
 
 export default db;
