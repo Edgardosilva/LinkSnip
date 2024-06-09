92fdc3bff713b21ee12f945cd0f152cf35c5f0f4
Edgardosilva <edgardosilva.es@gmail.com>
2024-06-09 15:57:47 -0400

    chore: Refactor database initialization and connection configuration

--- database/db.js
+++ database/db.js
@@ -3,20 +3,22 @@ import mysql from 'mysql2/promise';
 let db;
 
 async function initializeDatabase() {
-  try {
-    db = await mysql.createConnection({
-      host: process.env.DB_HOST || 'localhost',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_NAME || 'urlshortener',
-      connectTimeout: 10000 // Tiempo de espera en milisegundos
-    });
-    console.log('Conectado a MySQL');
-  } catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
+  if (!db) {
+    try {
+      db = await mysql.createConnection({
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || 'root',
+        database: process.env.DB_NAME || 'urlshortener',
+        connectTimeout: 10000 // Tiempo de espera en milisegundos
+      });
+      console.log('Conectado a MySQL');
+    } catch (error) {
+      console.error('Error al conectar a la base de datos:', error);
+      throw error;
+    }
   }
+  return db;
 }
 
-await initializeDatabase();
-
-export default db;
+export default initializeDatabase;
--- models/urlModel.js
+++ models/urlModel.js
@@ -1,11 +1,13 @@
-import db from '../database/db.js'
+import initializeDatabase from '../database/db.js';
 
 export const createURL = async (longUrl, shortUrl) => {
+  const db = await initializeDatabase();
   const query = "INSERT INTO urls (longUrl, shortUrl) VALUES (?, ?)";
   await db.execute(query, [longUrl, shortUrl]);
 };
 
 export const getURL = async (shortUrl) => {
+  const db = await initializeDatabase();
   const query = "SELECT longUrl FROM urls WHERE shortUrl = ?";
   const [results] = await db.execute(query, [shortUrl]);
   return results;
