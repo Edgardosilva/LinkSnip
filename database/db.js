1d0da6af5a38826b69905352f28517dcd731348e
Edgardosilva <edgardosilva.es@gmail.com>
2024-06-10 17:16:27 -0400

    Refactor database connection configuration to use @vercel/postgres

--- database/db.js
+++ database/db.js
@@ -1,12 +1,23 @@
-import { db } from "@vercel/postgres";
+// db.js
+import { Pool } from 'pg';
 
-export const initializeDatabase = async () => {
-    const client = db.connect({
-        host: process.env.POSTGRES_HOST,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DATABASE,
-    });
-    return client;
-};
+export const pool = new Pool({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    port: process.env.POSTGRES_PORT || 5432,  // Puerto por defecto es 5432
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+pool.on('connect', () => {
+    console.log('Connected to the PostgreSQL database');
+});
+
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
 
