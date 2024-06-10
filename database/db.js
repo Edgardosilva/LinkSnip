161f894a76babc442fa8d7ad2f9cb0f817740ce4
Edgardosilva <edgardosilva.es@gmail.com>
2024-06-10 18:17:03 -0400

    refactor: update database connection configuration to use MySQL URL

--- database/db.js
+++ database/db.js
@@ -1,16 +1,24 @@
 import mysql from 'mysql2/promise';
+import { URL } from 'url'; // Importa la clase URL para parsear la URL de conexión
 
 let db;
 
 export async function initializeDatabase() {
   try {
-    db = await mysql.createConnection({
-      host: process.env.DB_HOST || 'localhost',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_NAME || 'urlshortener',
+    // Parsear la URL de conexión
+    const url = new URL(process.env.RAILWAY_MYSQL_URL);
+    const dbOptions = {
+      host: url.hostname,
+      port: url.port,
+      user: url.username,
+      password: url.password,
+      database: url.pathname.substring(1), // Elimina el slash inicial del nombre de la base de datos
       connectTimeout: 10000 // Tiempo de espera en milisegundos
-    });
+    };
+
+    // Crear la conexión a la base de datos
+    db = await mysql.createConnection(dbOptions);
+    
     console.log('Conectado a MySQL');
     return db; // devuelve la conexión
   } catch (error) {
